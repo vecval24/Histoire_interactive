944c77ef99ac57564b3c7416d7a0fd9425e4db45
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2025-04-15 10:39:40 +0200

    add CRSF and baseUrl to fetch utils

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('test');
+  return view('test');
+});
+
+Route::prefix('api/v1/')->group(function () {
+  Route::get('/test', function () {
+    return response()->json(['message' => 'Hello, World from api!']);
+  });
+  Route::delete('/test', function () {
+    return response()->json(['message' => 'Deleting']);
+  });
 });

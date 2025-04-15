daf9918fa5bd4038cf6a33715abbfd391b26cab0
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2025-04-15 10:40:14 +0200

    add CRSF and baseUrl to fetch utils

---
+++ resources/js/pages/PageExample.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useFetchJson } from '@/composables/useFetchJson';
+
+const { data, error, isLoading } = useFetchJson('test');
+const { data: data2, error: error2} = useFetchJson({url: 'test', method: 'DELETE' });
+</script>
+
+<template>
+  <div>
+    <h1>Page Example</h1>
+    <p>{{ data }}</p>
+    <p>{{ data2 }}</p>
+    <p>{{ error2 }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

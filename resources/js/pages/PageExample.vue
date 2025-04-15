c36e194452096b4749f9b1093e46a04a635be46f
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2025-04-15 11:08:48 +0200

    Add fetch composable

---
+++ resources/js/pages/PageExample.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useFetchJson } from '@/composables/useFetchJson';
+
+const { data, error, isLoading } = useFetchJson('/api/v1/test');
+const { data: data2, error: error2} = useFetchJson({url: '/api/v1/test', method: 'DELETE' });
+</script>
+
+<template>
+  <div>
+    <h1>Page Example</h1>
+    <p>{{ data }}</p>
+    <p>{{ data2 }}</p>
+    <p>{{ error2 }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

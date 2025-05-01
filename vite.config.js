7ecfc9933bda40888cbddc2f33172d06ac06ac63
vecval24 <valentine.vecchi@heig-vd.ch>
2025-05-01 14:01:41 +0200

    Est-ce ça remarche enfin ?

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 // import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
@@ -27,6 +28,6 @@ export default defineConfig({
       },
 
     }),
-    // tailwindcss(),
+     tailwindcss(),
   ],
 });
